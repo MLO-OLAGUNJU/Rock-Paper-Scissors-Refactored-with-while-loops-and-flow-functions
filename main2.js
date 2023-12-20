d61d8e99814d9b619a1af93dff8054f8a1d836e3
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-20 12:39:19 +0100

    Rock, Paper, Scissors: Refactored with while Loop and an Array

--- main2.js
+++ main2.js
@@ -10,7 +10,27 @@ if (playGame) {
         playerOne === "paper" ||
         playerOne === "scissors"
       ) {
-        const computerChoice = Math.floor(Math.random());
+        const computerChoice = Math.floor(Math.random() * 3);
+        const rpsArray = ["rock", "paper", "scissors"];
+        const computer = rpsArray[computerChoice];
+
+        const result =
+          playerOne === computer
+            ? "Tie Game!"
+            : playerOne === "rock" && computer === "paper"
+            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+            : playerOne === "paper" && computer === "scissors"
+            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+            : playerOne === "scissors" && computer === "rock"
+            ? `playerOne: ${playerOne}\nComputer: ${computer} Computer wins!`
+            : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
+        alert(result);
+        playGame = confirm("Play again?");
+        if (!playGame)
+          alert("Okay maybe some other time.\nThanks for playing!");
+        continue;
+      } else {
+        alert("You didn't enter rock, paper or scissors.");
       }
     }
   }
