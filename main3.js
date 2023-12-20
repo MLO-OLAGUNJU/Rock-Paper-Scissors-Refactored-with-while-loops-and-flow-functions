f25be2eaacaec2a22e82863b636a22cb5b179275
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-20 13:24:36 +0100

    Rock, Paper, Scissors: Refactored with while loops and flow functions

--- main3.js
+++ main3.js
@@ -1,5 +1,40 @@
-// Rock, Paper, Scissors: REFACTORED
+// Rock, Paper, Scissors: REFACTORED with FUNCTIONS
 const initGame = () => {
   const startGame = confirm("Shall we play rock, paper, or scissors?");
   startGame ? playGame() : alert("Ok, maybe next time.");
+};
+
+//Game flow function
+
+const playGame = () => {
+  while (true) {
+    let playerChoice = getPlayerChoice();
+    playerChoice = formatPlayerChoice(playerChoice);
+    if (playerChoice === "") {
+      invalidChoice();
+      continue;
+    }
+
+    if (!playerChoice) {
+      decidedNotToPlay();
+      break;
+    }
+    playerChoice = evaluatePlayerChoice(playerChoice);
+    if (!playerChoice) {
+      invalidChoice();
+      continue;
+    }
+    const computerChoice = getComputerChoice();
+    const result = determineWinner(playerChoice, computerChoice);
+    displayResult(result);
+    if (askToPlayAgain()) {
+      continue;
+    } else {
+      thanksForPlaying();
+      break;
+    }
+  }
+};
+const getPlayerChoice = () => {
+  return prompt("Please enter rock, paper, or scissors");
 };
