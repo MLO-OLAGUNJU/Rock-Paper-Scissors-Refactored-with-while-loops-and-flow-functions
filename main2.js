f2c3954951918a0ca78a0c7a1c1cf1898944ae65
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-20 12:07:04 +0100

    Rock, Paper, Scissors: Refactored with while Loop and an Array

--- main2.js
+++ main2.js
@@ -1,2 +1,17 @@
 // Rock, Paper, Scissors: Refactored with while Loop and an Array
 let playGame = confirm("Shall we play rock, paper, or scissors");
+if (playGame) {
+  while (playGame) {
+    const playerChoice = prompt("Please enter rock, paper, scissors");
+    if (playerChoice || playerChoice === "") {
+      const playerChoice = playerChoice.trim().toLowerCase();
+      if (
+        playerOne === "rock" ||
+        playerOne === "paper" ||
+        playerOne === "scissors"
+      ) {
+        const computerChoice = Math.floor(Math.random());
+      }
+    }
+  }
+}
